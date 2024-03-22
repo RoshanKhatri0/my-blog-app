44c21aad1e111747b22cc8441238467223a3a491
RoshanKhatri0 <lastkhatri@gmail.com>
2024-03-22 11:11:10 +0545

    changes

--- app/components/BlogList.jsx
+++ app/components/BlogList.jsx
@@ -14,30 +14,25 @@ export default async function BlogList() {
 
     return (
         <>
-        <div className="container px-1 py-4">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 b-card">
-                            {posts.map(post => (
-                                <div className="b-col col">
-                                    <Link href={`/posts/${post.id}`}>
-                                        <div className="cardd" key={post.id}>
-                                            <h3 className="cardd__title">{truncateTitle(post.title)}
-                                            </h3>
-                                            <p className="cardd__content">{truncateBody(post.body)} </p>
-
-                                            <div className="cardd__arrow">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="15" width="15">
-                                                    <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z"></path>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </Link>
+            <div className="container px-1 py-4">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 b-card">
+                    {posts.map(post => (
+                        <div className="b-col col" key={post.id}>
+                            <Link href={`/posts/${post.id}`}>
+                                <div className="cardd">
+                                    <h3 className="cardd__title">{truncateTitle(post.title)}</h3>
+                                    <p className="cardd__content">{truncateBody(post.body)}</p>
+                                    <div className="cardd__arrow">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="15" width="15">
+                                            <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z"></path>
+                                        </svg>
+                                    </div>
                                 </div>
-                            ))}
-
+                            </Link>
                         </div>
-        </div>
-            
+                    ))}
+                </div>
+            </div>
         </>
-
     );
 }
--- app/posts/not-found.jsx
+++ app/posts/not-found.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from "next/link"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -5,12 +6,13 @@ export default function NotFound() {
   return (
     <div className="container text-center my-5">
       <h1>We hit a Brick Wall</h1>
-    <img
-      src="https://i.pinimg.com/564x/4e/19/c2/4e19c2d8da38136202aa53345057f601.jpg"
-      style={{ height: '400px' }}
-      alt="Not Found"
-      className="img-fluid my-2"
-    />
+      <Image
+        src="https://i.pinimg.com/564x/4e/19/c2/4e19c2d8da38136202aa53345057f601.jpg"
+        alt="Not Found"
+        width={400}
+        height={400}
+        className="img-fluid my-2"
+      />
     <p className="lead">
     We could not find the blog you were looking for.
     </p>
